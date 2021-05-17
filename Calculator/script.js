d902c521cc73f87fd7dfb352926c0ff543bfb200
Antrang Agrawal <antrang6112@gmail.com>
2021-05-17 09:35:32 -0400

    Calaculator

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+function Operations(j) {
+    let result = 0;
+    switch(j){
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "*":
+            result = a*b;
+            break;
+        case "/":
+            result = a/b;
+            break;
+    }
+    return result;
+}
+
+
+
